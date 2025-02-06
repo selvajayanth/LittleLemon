37a7e45fbff5d40e88b17894513761951b6f0774
selvajayanth <selvakumarjayanth@gmail.com>
2025-02-06 17:17:39 +0530

    DB configurations and models created

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PORT': '3306',
+        'PASSWORD': 'root',
+        'NAME': 'restaurant',
     }
 }
 
